72ccd7932552ba7d47c7d9000d33a08f0fa2a7e9
Andrzejowski Grzegorz <grzegorz.andrzejowski@gmail.com>
2018-02-03 17:37:17 +0100

    Added CustomDirectionList class

---
+++ Turmity/CustomDirectionList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Turmity
+{
+    class CustomDirectionList : DirectionList
+    {
+        public CustomDirectionList(int[][] customList) => Vectors = customList;
+    }
+}
