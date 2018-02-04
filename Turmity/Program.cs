cbc27da5c436e32f4ad8752e7b323e53967a050a
Andrzejowski Grzegorz <grzegorz.andrzejowski@gmail.com>
2018-02-04 18:30:06 +0100

    Basic output to file

--- Turmity/Program.cs
+++ Turmity/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Turmity
 {
@@ -15,7 +16,26 @@ namespace Turmity
             System.Console.Clear();
             Simulation sim = new Simulation(settings.Head, settings.Steps, settings.Width, settings.Height);
             sim.Run();
-            System.Console.Write(sim.Result);
+            System.Console.WriteLine("Wyświetlić wynik? t/n");
+            char key;
+            do
+            {
+                key = System.Console.ReadKey().KeyChar;
+            } while (key != 't' && key != 'n');
+            if (key == 't') System.Console.WriteLine(sim.Result);
+            System.Console.WriteLine("Zapis do pliku? t/n");
+            do
+            {
+                key = System.Console.ReadKey().KeyChar;
+            } while (key != 't' && key != 'n');
+            if (key=='t')
+            {
+                System.Console.Write("Podaj nazwę pliku");
+                string filename = System.Console.ReadLine();
+                string path = System.Environment.CurrentDirectory;
+                File.WriteAllText("output\\" + filename + ".txt", sim.Result);
+                System.Console.WriteLine("Zapisano: " + path + "\\output\\" + filename);
+            }
             System.Console.ReadKey();
         }
     }
